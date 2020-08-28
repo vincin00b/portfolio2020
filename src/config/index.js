4318075e636311caa69f40392655dc3feb442a5f
vincin00b <vincineuze@gmail.com>
2020-08-28 18:25:42 +0200

    1ercommit

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     author: "@konstantinmuenster",
-    siteTitle: "Gatsby Starter Portfolio Minimal",
+    siteTitle: "vincent",
     siteShortTitle: "Gatsby", // Used as logo text in header, footer, and splash screen
     siteDescription: "A modern one-page portfolio with a clean yet expressive design.",
     siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
